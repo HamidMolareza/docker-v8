19efa33836371fe14b9900e44f9f4cf4412d8c45
Hamid Molareza <kitm.hamid@gmail.com>
2023-04-17 22:07:50 +0330

    fix: improve `log_error`

--- tests/test_lib/test_utility.py
+++ tests/test_lib/test_utility.py
@@ -5,7 +5,7 @@ from on_rails import (Result, ValidationError, assert_error_detail,
                       assert_result, assert_result_with_type)
 
 from docker_entrypoint._libs.ResultDetails.FailResult import FailResult
-from docker_entrypoint._libs.utility import log_result
+from docker_entrypoint._libs.utility import log_error, log_result
 from tests._helpers import get_logger
 
 
@@ -24,7 +24,8 @@ class TestUtility(unittest.TestCase):
 
         assert_result_with_type(self, result, expected_success=False, expected_detail_type=ValidationError)
         assert_error_detail(self, result.detail, expected_title="The result parameter is not valid.",
-                            expected_message="The result parameter is required and must be an instance of Result.", expected_code=400)
+                            expected_message="The result parameter is required and must be an instance of Result.",
+                            expected_code=400)
 
     def test_log_result_give_success(self):
         logger, logging_stream = get_logger()
@@ -64,6 +65,55 @@ class TestUtility(unittest.TestCase):
                       "\tRepository: No Data!\n"
                       "\tReport Bug: No Data!",
                       logging_stream.getvalue())
+
+    # endregion
+
+    # region log_error
+
+    def test_log_error_not_give_logger(self):
+        result = log_error(None, None)
+
+        assert_result_with_type(self, result, expected_success=False, expected_detail_type=ValidationError)
+        assert_error_detail(self, result.detail, expected_title="One or more validation errors occurred",
+                            expected_message="The logger is required.", expected_code=400)
+
+    def test_log_error_give_none_result(self):
+        result = log_error(logging.getLogger(), None)
+
+        assert_result_with_type(self, result, expected_success=False, expected_detail_type=ValidationError)
+        assert_error_detail(self, result.detail, expected_title="The result parameter is not valid.",
+                            expected_message="The result parameter is required and must be an instance of Result.",
+                            expected_code=400)
+
+    def test_log_error_give_success_result(self):
+        result = Result.ok()
+        func_result = log_error(logging.getLogger(), result)
+
+        assert_result_with_type(self, func_result, expected_success=False, expected_detail_type=ValidationError)
+        assert_error_detail(self, func_result.detail, expected_title="One or more validation errors occurred",
+                            expected_message="Expected failure result but got success result!",
+                            expected_code=400, expected_more_data=[result])
+
+    def test_log_error(self):
+        logger, logging_stream = get_logger()
+
+        func_result = log_error(logger, Result.fail(ValidationError()))
+        assert_result(self, target_result=func_result, expected_success=True)
+
+        self.assertIn("[ERROR] An error occurred:\n"
+                      "Title: One or more validation errors occurred\n"
+                      "Code: 400\n"
+                      "Stack trace:",
+                      logging_stream.getvalue())
+        self.assertIn("[INFO] Please report this error to help others who use this program.\n"
+                      "Support:\n"
+                      "\tMaintainer: No Data!\n"
+                      "\tDocker Version: latest\n"
+                      "\tBuild Date: No Data!\n"
+                      "\tRepository: No Data!\n"
+                      "\tReport Bug: No Data!",
+                      logging_stream.getvalue())
+        print(logging_stream.getvalue())
 
     # endregion
 
